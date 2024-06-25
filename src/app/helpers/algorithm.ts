3d9577021f756ec3362f5a25b18a88759750875a
aviad <aviad@arionfund.com>
2024-06-25 20:22:40 +0300

    Fix

--- src/app/helpers/algorithm.ts
+++ src/app/helpers/algorithm.ts
@@ -27,23 +27,23 @@ export interface GuardsOptions {
   normalGuards: number;
 }
 
-const HOURS_IN_DAY = 23;
-const SIX_DAYS_HOURS = HOURS_IN_DAY * 6;
+const HOURS_IN_DAY = 24;
+const SIX_DAYS_HOURS = HOURS_IN_DAY * 1;
 
 const MISSIONS: Record<string, MissionDetails> = {
   [Missions.RASAR]: { time: [9, 22], totalGuards: 10 },
   [Missions.LOGISTIKA]: { time: [9, 22], totalGuards: 5 },
   [Missions.KITCHEN]: { time: [4, 22], totalGuards: 24 },
-  [Missions.NASHKIA]: { time: [0, 23] },
-  [Missions.SHINPE]: { time: [0, 23] },
-  [Missions.MECHAPE]: { time: [0, 23] },
-  [Missions.ACHORI]: { time: [0, 23] },
-  [Missions.TAYSON1]: { time: [0, 23] },
-  [Missions.TAYSON2]: { time: [0, 23] },
-  [Missions.JANKIA]: { time: [0, 23] },
-  [Missions.SHAG_BUNKER]: { time: [0, 23] },
-  [Missions.MIGDAL_BUNKER]: { time: [0, 23] },
-  [Missions.BUNKER]: { time: [0, 23] },
+  [Missions.NASHKIA]: { time: [0, 24] },
+  [Missions.SHINPE]: { time: [0, 24] },
+  [Missions.MECHAPE]: { time: [0, 24] },
+  [Missions.ACHORI]: { time: [0, 24] },
+  [Missions.TAYSON1]: { time: [0, 24] },
+  [Missions.TAYSON2]: { time: [0, 24] },
+  [Missions.JANKIA]: { time: [0, 24] },
+  [Missions.SHAG_BUNKER]: { time: [0, 24] },
+  [Missions.MIGDAL_BUNKER]: { time: [0, 24] },
+  [Missions.BUNKER]: { time: [0, 24] },
   [Missions.SG_BUNKER]: { time: [7, 22] },
 };
 
@@ -61,13 +61,24 @@ const ThreeHoursShift = [
   Missions.SG_BUNKER,
 ];
 
+const minusOneOrReset = (num: number, originalNum: number) => (num === 1 ? originalNum : num - 1);
+
 export const generateSchedule = (options: GuardsOptions) => {
-  const hours = Array.from({ length: SIX_DAYS_HOURS }, (_, i) => i);
+  let copiedOptions = { ...options };
+  let {
+    normalGuards,
+    guardsCanGuardOnlyAtNight,
+    guardsCanGuardOnlyInShinPe,
+    guardsCantGoToKitchen,
+  } = copiedOptions;
+  const hours = Array.from({ length: HOURS_IN_DAY }, (_, i) => i);
 
   const guardList: Record<Missions | string, string[] | number>[] = [];
   const missionsEndHour = new Map<string, number>();
 
   for (const hour of hours) {
+    console.log("hour", hour);
+
     const dayTime = hour % HOURS_IN_DAY;
     const activeMissions = Object.entries(MISSIONS).filter(([_, { time }]) => {
       const [start, end] = time;
@@ -75,7 +86,7 @@ export const generateSchedule = (options: GuardsOptions) => {
     });
 
     guardList.push({
-      day: Math.floor(hour / HOURS_IN_DAY),
+      day: Math.floor(hour / HOURS_IN_DAY) + 1,
       hour: dayTime,
     });
 
@@ -86,6 +97,7 @@ export const generateSchedule = (options: GuardsOptions) => {
         if (missionEndHour > dayTime) {
           return (guardListItem[missionName] = guardList[guardList.length - 2][missionName]);
         }
+
         missionsEndHour.delete(missionName);
       }
 
@@ -95,21 +107,30 @@ export const generateSchedule = (options: GuardsOptions) => {
       let neededGuards = totalGuards || 1;
       while (neededGuards > 0) {
         let guardName = "";
-        if ((dayTime >= 19 || dayTime < 7) && options.guardsCanGuardOnlyAtNight > 0) {
-          guardName = `יכול לשמור רק בלילה ${options.guardsCanGuardOnlyAtNight}`;
-          options.guardsCanGuardOnlyAtNight--;
+        if ((dayTime >= 19 || dayTime < 7) && guardsCanGuardOnlyAtNight > 0) {
+          guardName = `יכול לשמור רק בלילה ${guardsCanGuardOnlyAtNight}`;
+          guardsCanGuardOnlyAtNight = minusOneOrReset(
+            guardsCanGuardOnlyAtNight,
+            options.guardsCanGuardOnlyAtNight,
+          );
           neededGuards--;
-        } else if (missionName === Missions.SHINPE && options.guardsCanGuardOnlyInShinPe > 0) {
-          guardName = `יכול לשמור רק בשינפה ${options.guardsCanGuardOnlyInShinPe}`;
-          options.guardsCanGuardOnlyInShinPe--;
+        } else if (missionName === Missions.SHINPE && guardsCanGuardOnlyInShinPe > 0) {
+          guardName = `יכול לשמור רק בשינפה ${guardsCanGuardOnlyInShinPe}`;
+          guardsCanGuardOnlyInShinPe = minusOneOrReset(
+            guardsCanGuardOnlyInShinPe,
+            options.guardsCanGuardOnlyInShinPe,
+          );
           neededGuards--;
-        } else if (missionName !== Missions.KITCHEN && options.guardsCantGoToKitchen > 0) {
-          guardName = `לא יכול ללכת למטבח ${options.guardsCantGoToKitchen}`;
-          options.guardsCantGoToKitchen--;
+        } else if (missionName !== Missions.KITCHEN && guardsCantGoToKitchen > 0) {
+          guardName = `לא יכול ללכת למטבח ${guardsCantGoToKitchen}`;
+          guardsCantGoToKitchen = minusOneOrReset(
+            guardsCantGoToKitchen,
+            options.guardsCantGoToKitchen,
+          );
           neededGuards--;
-        } else if (options.normalGuards > 0) {
-          guardName = `רגיל ${options.normalGuards}`;
-          options.normalGuards--;
+        } else if (normalGuards > 0) {
+          guardName = `רגיל ${normalGuards}`;
+          normalGuards = minusOneOrReset(normalGuards, options.normalGuards);
           neededGuards--;
         } else {
           neededGuards--;
